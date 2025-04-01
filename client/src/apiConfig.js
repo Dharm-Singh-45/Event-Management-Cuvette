303b9ae0db48a2f5a7eb3f1f9391c14a21ae030c
Dharm-Singh-45 <dharmjat45@gmail.com>
2025-04-02 00:07:03 +0530

    api added

--- client/src/apiConfig.js
+++ client/src/apiConfig.js
@@ -1,4 +1,4 @@
 
 
-export const BASE_URL = "http://localhost:5000/api";
-// export const BASE_URL = "https://event-management-cuvette.onrender.com/api";
+// export const BASE_URL = "http://localhost:5000/api";
+export const BASE_URL = "https://event-management-cuvette.onrender.com/api";
